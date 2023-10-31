d9a26ca94a8e8256737af2e5ab287a21c7b9d12e
Rizwan Ahmad <113388795+Riz1981@users.noreply.github.com>
2023-10-31 11:45:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+//four lines with console log
+let sum = 5 + 3;
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
---
+++ index.ts
@@ -0,0 +1,7 @@
+//four lines with console log
+let sum: number =5+3;
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
+
